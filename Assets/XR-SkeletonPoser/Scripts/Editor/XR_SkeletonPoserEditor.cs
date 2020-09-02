a63293ee65d406c26026b93075c014fccc1f521d
yellowyears <yellowyears200@gmail.com>
2020-09-02 08:25:48 +0100

    WIP: Implement show / hide preview models
    Needs fixing: Hand gets destroyed when deselecting the XRSkeletonPoser

--- Assets/XR-SkeletonPoser/Scripts/Editor/XR_SkeletonPoserEditor.cs
+++ Assets/XR-SkeletonPoser/Scripts/Editor/XR_SkeletonPoserEditor.cs
@@ -5,6 +5,30 @@ using UnityEditor;
 public class XR_SkeletonPoserEditor : Editor
 {
     
+    private XR_SkeletonPoser _poser;
+    
+    private GameObject tempLeft = null;
+    private GameObject tempRight = null;
+
+    private bool showLeftIsPressed;
+    private bool hideLeftIsPressed;
+
+    private bool showRightIsPressed;
+    private bool hideRightIsPressed;
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+        _poser = (XR_SkeletonPoser) target;
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable");
+        DestroyImmediate(tempLeft);
+        DestroyImmediate(tempRight);
+    }
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
@@ -24,13 +48,62 @@ public class XR_SkeletonPoserEditor : Editor
             
             // Preview button
             
+            EditorGUILayout.LabelField("Do not unselect the gameobject, it will destroy the hand! This isn't intended behaviour, fix soon™!");
+            
+            EditorGUILayout.BeginHorizontal("box");
+            
+            showLeftIsPressed = GUILayout.Button("Show Left", "box");
+            if(showLeftIsPressed && tempLeft == null)
+            {
+                tempLeft = _poser.ShowLeftPreview();
+            }
+            else if (showLeftIsPressed && tempLeft != null)
+            {
+                return;
+            }
+            
+            hideLeftIsPressed = GUILayout.Button("Destroy Left", "box");
+            if(hideLeftIsPressed && tempLeft != null)
+            {
+                tempLeft = _poser.DestroyLeftPreview(tempLeft);
+            }
+            else if (hideLeftIsPressed && tempLeft == null)
+            {
+                return;
+            }
+
+            EditorGUILayout.Space(20);
+
+            showRightIsPressed = GUILayout.Button("Show Right", "box");
+            if(showRightIsPressed && tempRight == null)
+            {
+                tempRight = _poser.ShowLeftPreview();
+            }
+            else if (showRightIsPressed && tempRight != null)
+            {
+                return;
+            }
+            
+            hideRightIsPressed = GUILayout.Button("Destroy Right", "box");
+            if(hideRightIsPressed && tempRight != null)
+            {
+                tempRight = _poser.DestroyRightPreview(tempRight);
+            }
+            else if (hideRightIsPressed && tempRight == null)
+            {
+                return;
+            }
+
+            
+            EditorGUILayout.EndHorizontal();
+
             // Reset Pose button
-            
+
             // Save pose button
-            
+
             // Load pose button
-            
+
         }
     }
-    
+
 }
--- Assets/XR-SkeletonPoser/Scripts/XR_SkeletonPoser.cs
+++ Assets/XR-SkeletonPoser/Scripts/XR_SkeletonPoser.cs
@@ -1,8 +1,36 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 public class XR_SkeletonPoser : MonoBehaviour
 {
 
     [SerializeField] private XR_SkeletonPose activePose;
+
+    public GameObject leftHand = null;
+    public GameObject rightHand = null;
+
+    // Functions used by XR_SkeletonPoserEditor.
+    
+    public GameObject ShowLeftPreview()
+    {
+        return Instantiate(leftHand);
+    }
+
+    public GameObject DestroyLeftPreview(GameObject obj)
+    {
+        DestroyImmediate(obj);
+        return null;
+    }
+
+    public GameObject ShowRightPreview()
+    {
+        return Instantiate(rightHand);
+    }
+
+    public GameObject DestroyRightPreview(GameObject obj)
+    {
+        DestroyImmediate(obj);
+        return null;
+    }
     
 }
