e7bd0471c43a6d9fb396640104366e97c6aa7cbb
yellowyears <yellowyears200@gmail.com>
2020-09-10 21:31:51 +0100

    WIP: Attempt conversion of Object to GameObject
    Not working

--- Assets/XR-SkeletonPoser/Scripts/Editor/XR_SkeletonPoserEditor.cs
+++ Assets/XR-SkeletonPoser/Scripts/Editor/XR_SkeletonPoserEditor.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 [CustomEditor(typeof(XR_SkeletonPoser))]
 public class XR_SkeletonPoserEditor : Editor
 {
+    private XR_SkeletonPoser _poser = null;
+    
     private GameObject _tempLeft;
     private bool _toggleLeftHand;
 
-    private SerializedObject _targetObject;
+    private SerializedObject _targetObject = null;
     private SerializedProperty _propertyLeftToggled = null;
-    private SerializedProperty _propertyTempLeft = null;
+    // private SerializedProperty _propertyTempLeft = null;
 
     private void OnEnable()
     {
         _targetObject = serializedObject;
+        
+        GameObject poserGameObject = _targetObject.targetObject as GameObject;
+        // Returns null, bug
+        _poser = poserGameObject.GetComponent<XR_SkeletonPoser>();
+
         _propertyLeftToggled = _targetObject.FindProperty("leftToggled");
     }
 
@@ -39,9 +46,9 @@ public class XR_SkeletonPoserEditor : Editor
             _toggleLeftHand = EditorGUILayout.Toggle("Toggle Left", _toggleLeftHand);
             if (_toggleLeftHand)
             {
-                if (!_propertyLeftToggled.boolValue) _tempLeft = _targetObject.ShowLeftPreview();
+                if (!_propertyLeftToggled.boolValue) _tempLeft = _poser.ShowLeftPreview();
             
-                _tempLeft.transform.parent = _targetObject.targetObject;
+                _tempLeft.transform.parent = _poser.gameObject.transform;
                 _tempLeft.transform.localPosition = Vector3.zero;
                 _tempLeft.transform.localRotation = Quaternion.identity;
             
@@ -49,7 +56,7 @@ public class XR_SkeletonPoserEditor : Editor
             }
             else
             {
-                if (_propertyLeftToggled.boolValue) _targetObject.DestroyLeftPreview(_tempLeft);
+                if (_propertyLeftToggled.boolValue) _poser.DestroyLeftPreview(_tempLeft);
                 _propertyLeftToggled.boolValue = false;
             }
         }
