cfe0683e4a8d905a6dd803c99f223918c6d58f47
yellowyears <yellowyears200@gmail.com>
2020-09-17 21:11:26 +0100

    WIP: Add XR_SkeletonPoseInteractor

---
+++ Assets/XR-SkeletonPoser/Scripts/XR_SkeletonPoseInteractor.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine;
+
+namespace yellowyears.SkeletonPoser
+{
+    public class XR_SkeletonPoseInteractor : XRDirectInteractor
+    {
+
+        public Transform[] _handBones = null;
+
+        private void SetPose(XR_SkeletonPose pose)
+        {
+            // Get hand bones
+            
+            // Bug: Gets attach models, and also the controller. Need to remove everything that isn't under the glove model
+            _handBones = GetComponentsInChildren<Transform>().ToArray();
+
+            Vector3[] posePos = pose.leftBonePositions;
+            Quaternion[] poseRot = pose.leftBoneRotations;
+            
+            // Vector3[] rightPosePos = pose.rightBonePositions;
+            // Quaternion[] rightPoseRot = pose.rightBoneRotations;
+
+            // Set values to loaded pose
+            for (int i = 0; i < _handBones.Length; i++)
+            {
+                _handBones[i].localPosition = posePos[i];
+                _handBones[i].localRotation = poseRot[i];
+            }
+        }
+        
+        protected override void OnSelectEnter(XRBaseInteractable interactable)
+        {
+            base.OnSelectEnter(interactable);
+            
+            if(interactable.TryGetComponent(out XR_SkeletonPoser poser))
+            {
+                XR_SkeletonPose pose = poser.GetLoadedPose();
+                
+                SetPose(pose);
+            }
+        }
+    }
+}
