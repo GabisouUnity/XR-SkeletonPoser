845e99513eb8ec7261e179681e256529f0486341
yellowyears <yellowyears200@gmail.com>
2020-09-22 17:08:47 +0100

    Extract button code to methods

--- Assets/XR-SkeletonPoser/Scripts/Editor/XR_SkeletonPoserEditor.cs
+++ Assets/XR-SkeletonPoser/Scripts/Editor/XR_SkeletonPoserEditor.cs
@@ -76,9 +76,7 @@ namespace yellowyears.SkeletonPoser
 
                 if (_propertyShowPoseEditor.boolValue)
                 {
-                   EditorGUILayout.BeginHorizontal();
-
-                   EditorGUI.BeginDisabledGroup(_poser.leftHand == null);
+                   EditorGUILayout.BeginHorizontal(); EditorGUI.BeginDisabledGroup(_poser.leftHand == null);
                    
                    if (!_propertyShowLeft.boolValue)
                    {
@@ -134,84 +132,36 @@ namespace yellowyears.SkeletonPoser
                        }
                    }
 
-                   EditorGUI.EndDisabledGroup();
-                   
-                   EditorGUILayout.EndHorizontal();
-
-                   // TODO: copy pose data to opposite hand
-                   
-                   // EditorGUILayout.BeginHorizontal();
-                   //
-                   // if (GUILayout.Button("Copy Left Pose to right hand"))
-                   // {
-                   //     
-                   // }
-                   //
-                   // if (GUILayout.Button("Copy Right Pose to left hand"))
-                   // {
-                   //     
-                   // }
-                   //
-                   // EditorGUILayout.EndHorizontal();
+                   EditorGUI.EndDisabledGroup(); EditorGUILayout.EndHorizontal();
                    
                    EditorGUILayout.BeginHorizontal();
                    
-                   EditorGUILayout.PropertyField(_propertyActivePose);
+                   if (GUILayout.Button("Copy Left Pose to right hand"))
+                   {
+                       CopyToRight();
+                   }
                    
-                   // Load pose button
+                   if (GUILayout.Button("Copy Right Pose to left hand"))
+                   {
+                       CopyToLeft();
+                   }
+                   
+                   EditorGUILayout.EndHorizontal();
+                   
+                   EditorGUILayout.BeginHorizontal();
+                   
+                   // Create field in editor for active pose (also referred to as loaded pose)
+                   EditorGUILayout.PropertyField(_propertyActivePose);
 
-                   // Grey it out if hands aren't active
+                   // Grey it out if hands aren't active and there is no loaded pose
                    EditorGUI.BeginDisabledGroup(_propertyShowLeft.boolValue == false && _propertyShowRight.boolValue == false || _poser.GetLoadedPose() == null);
 
                    if (GUILayout.Button("Load Pose"))
                    {
-                       var loadedPose = _poser.GetLoadedPose();
-                       
-                       var leftBonePositions = loadedPose.leftBonePositions;
-                       var leftBoneRotations = loadedPose.leftBoneRotations;
-
-                       var rightBonePositions = loadedPose.rightBonePositions;
-                       var rightBoneRotations = loadedPose.rightBoneRotations;
-
-                       if (_leftGameObject != null)
-                       {
-                           var leftTransforms = _leftGameObject.GetComponentsInChildren<Transform>().ToArray();
-                           
-                           // Set left values to loaded pose
-                           for (int i = 0; i < leftBonePositions.Length; i++)
-                           {
-                               leftTransforms[i].localPosition = leftBonePositions[i];
-                           }
-                           
-                           for (int i = 0; i < leftBoneRotations.Length; i++)
-                           {
-                               leftTransforms[i].localRotation = leftBoneRotations[i];
-                           }
-                       }
-                       
-                       // Declare right values
-                       
-                       if (_rightGameObject != null)
-                       {
-                           var rightTransforms = _rightGameObject.GetComponentsInChildren<Transform>().ToArray();
-                           
-                           for (int i = 0; i < rightBonePositions.Length; i++)
-                           {
-                               rightTransforms[i].localPosition = rightBonePositions[i];
-                           }
-
-                           for (int i = 0; i < rightBoneRotations.Length; i++)
-                           {
-                               rightTransforms[i].localRotation = rightBoneRotations[i];
-                           }
-                       }
-
+                       LoadPose();
                    }
                    
-                   // Grey it out if hands aren't active
-                   EditorGUI.EndDisabledGroup();
-
-                   EditorGUILayout.EndHorizontal();
+                   EditorGUI.EndDisabledGroup(); EditorGUILayout.EndHorizontal();
                    
                    EditorGUILayout.Space();
                    
@@ -221,74 +171,23 @@ namespace yellowyears.SkeletonPoser
                    // Grey it out if hands aren't active
                    EditorGUI.BeginDisabledGroup(_propertyShowLeft.boolValue == false || _propertyShowRight.boolValue == false);
                    
-                   // Save pose
-                   
                    if (GUILayout.Button("Save Pose"))
                    {
-
-                       // Todo: Only overwrite the data from the active hand(s). Although it might not be possible?
-
-                       if (_propertyTempLeft.objectReferenceValue != null)
-                       {
-                           newPose.leftBonePositions = _poser.GetBonePositions(_propertyTempLeft.objectReferenceValue as GameObject);
-                           newPose.leftBoneRotations = _poser.GetBoneRotations(_propertyTempLeft.objectReferenceValue as GameObject);
-                       }
-
-                       if (_propertyTempRight.objectReferenceValue != null)
-                       {
-                           newPose.rightBonePositions = _poser.GetBonePositions(_propertyTempRight.objectReferenceValue as GameObject);
-                           newPose.rightBoneRotations = _poser.GetBoneRotations(_propertyTempRight.objectReferenceValue as GameObject);
-                       }
-
-                       // Set pose to new pose data to avoid the need for reassignment after saving
-                       _poser.activePose = newPose;
-
-                       if (!AssetDatabase.IsValidFolder("Assets/XRPoses"))
-                       {
-                           // Folder doesn't exist, create new
-                           AssetDatabase.CreateFolder("Assets", "XRPoses");
-                           AssetDatabase.CreateAsset(newPose, $"Assets/XRPoses/{_poser.gameObject.name}.asset");
-                       }
-                       else
-                       {
-                           // Folder exists
-                           AssetDatabase.CreateAsset(newPose, $"Assets/XRPoses/{_poser.gameObject.name}.asset");
-                       }
+                       SavePose(newPose);
                    }
                    
                    EditorGUI.EndDisabledGroup();
-                   
-                   // Reset Pose
-                   
+
                    EditorGUI.BeginDisabledGroup(_propertyShowLeft.boolValue == false && _propertyShowRight.boolValue == false || _poser.GetLoadedPose() == null);
                    
                    if (GUILayout.Button("Reset Pose"))
                    {
                        // Make sure we warn the user before they reset their pose
-                       if (EditorUtility.DisplayDialog("Reset Pose?", "Are you sure you want to do this? You will lose your pose on this object!", "Yes", "No"))
+                       if (EditorUtility.DisplayDialog("Reset Pose?",
+                           "Are you sure you want to do this? You will lose your pose on this object!", "Yes", "No"))
                        {
                            // They are sure, reset pose
-                           
-                           // Set pose to new pose data to avoid the need for reassignment after saving the file
-                           _poser.activePose = _defaultPose;
-
-                           // Save and overwrite
-                           if (!AssetDatabase.IsValidFolder("Assets/XRPoses"))
-                           {
-                               // Folder doesn't exist, create new
-                               AssetDatabase.CreateFolder("Assets", "XRPoses");
-                               
-                               // Overwrite the pose with a default pose
-                               AssetDatabase.CreateAsset(_defaultPose, $"Assets/XRPoses/{_poser.gameObject.name}.asset");
-                           }
-                           else
-                           {
-                               // Folder exists
-                               
-                               // Overwrite the pose with a default pose
-                               
-                               AssetDatabase.CreateAsset(_defaultPose, $"Assets/XRPoses/{_poser.gameObject.name}.asset");
-                           }
+                           ResetPose(newPose); // Reset new pose instance to default pose
                        }
                    }
                    
@@ -296,6 +195,117 @@ namespace yellowyears.SkeletonPoser
                 }
                 
                 EditorGUILayout.EndFoldoutHeaderGroup();
+            }
+        }
+
+        private void CopyToRight()
+        {
+            // Copy left pose data to right
+        }
+
+        private void CopyToLeft()
+        {
+            // Copy right pose data to left
+        }
+        
+        private void SavePose(XR_SkeletonPose newPose)
+        {
+            // Todo: Only overwrite the data from the active hand(s). Although it might not be possible?
+
+            if (_propertyTempLeft.objectReferenceValue != null)
+            {
+                newPose.leftBonePositions = _poser.GetBonePositions(_propertyTempLeft.objectReferenceValue as GameObject);
+                newPose.leftBoneRotations = _poser.GetBoneRotations(_propertyTempLeft.objectReferenceValue as GameObject);
+            }
+
+            if (_propertyTempRight.objectReferenceValue != null)
+            {
+                newPose.rightBonePositions = _poser.GetBonePositions(_propertyTempRight.objectReferenceValue as GameObject);
+                newPose.rightBoneRotations = _poser.GetBoneRotations(_propertyTempRight.objectReferenceValue as GameObject);
+            }
+
+            // Set pose to new pose data to avoid the need for reassignment after saving
+            _poser.activePose = newPose;
+
+            if (!AssetDatabase.IsValidFolder("Assets/XRPoses"))
+            {
+                // Folder doesn't exist, create new
+                AssetDatabase.CreateFolder("Assets", "XRPoses");
+                AssetDatabase.CreateAsset(newPose, $"Assets/XRPoses/{_poser.gameObject.name}.asset");
+            }
+            else
+            {
+                // Folder exists
+                AssetDatabase.CreateAsset(newPose, $"Assets/XRPoses/{_poser.gameObject.name}.asset");
+            }
+        }
+
+        private void ResetPose(XR_SkeletonPose pose)
+        {
+            // Set pose to new pose data to avoid the need for reassignment after saving the file
+            pose = _defaultPose;
+            _poser.activePose = pose;
+
+            // Save and overwrite
+            if (!AssetDatabase.IsValidFolder("Assets/XRPoses"))
+            {
+                // Folder doesn't exist, create new
+                AssetDatabase.CreateFolder("Assets", "XRPoses");
+
+                // Overwrite the pose with a default pose
+                AssetDatabase.CreateAsset(_defaultPose, $"Assets/XRPoses/{_poser.gameObject.name}.asset");
+            }
+            else
+            {
+                // Folder exists
+
+                // Overwrite the pose with a default pose
+
+                AssetDatabase.CreateAsset(_defaultPose, $"Assets/XRPoses/{_poser.gameObject.name}.asset");
+            }
+        }
+
+        private void LoadPose()
+        {
+            var loadedPose = _poser.GetLoadedPose();
+
+            var leftBonePositions = loadedPose.leftBonePositions;
+            var leftBoneRotations = loadedPose.leftBoneRotations;
+
+            var rightBonePositions = loadedPose.rightBonePositions;
+            var rightBoneRotations = loadedPose.rightBoneRotations;
+
+            if (_leftGameObject != null)
+            {
+                var leftTransforms = _leftGameObject.GetComponentsInChildren<Transform>().ToArray();
+
+                // Set left values to loaded pose
+                for (int i = 0; i < leftBonePositions.Length; i++)
+                {
+                    leftTransforms[i].localPosition = leftBonePositions[i];
+                }
+
+                for (int i = 0; i < leftBoneRotations.Length; i++)
+                {
+                    leftTransforms[i].localRotation = leftBoneRotations[i];
+                }
+            }
+
+            // Declare right values
+
+            if (_rightGameObject != null)
+            {
+                var rightTransforms = _rightGameObject.GetComponentsInChildren<Transform>().ToArray();
+
+                for (int i = 0; i < rightBonePositions.Length; i++)
+                {
+                    rightTransforms[i].localPosition = rightBonePositions[i];
+                }
+
+                for (int i = 0; i < rightBoneRotations.Length; i++)
+                {
+                    rightTransforms[i].localRotation = rightBoneRotations[i];
+                }
             }
         }
     }    
