28db425b89d362fc4110e29e5f39a425dfdafad2
yellowyears <yellowyears200@gmail.com>
2020-09-29 17:06:30 +0100

    Add XR_SkeletonPoserSettings

---
+++ Assets/XR-SkeletonPoser/Scripts/XR_SkeletonPoserSettings.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace yellowyears.SkeletonPoser
+{
+    public class XR_SkeletonPoserSettings : ScriptableObject
+    {
+
+        private static XR_SkeletonPoserSettings _instance;
+
+        public static XR_SkeletonPoserSettings instance
+        {
+            get
+            {
+                LoadInstance();
+
+                return instance;
+            }
+        }
+
+        private static void LoadInstance()
+        {
+            if (_instance != null) return;
+            
+            // Load settings, from a folder in Assets/XRPoses called "Resources"
+            _instance = Resources.Load<XR_SkeletonPoserSettings>("XRPoses");
+
+            if (_instance != null) return;
+                
+            // If it still is null, then it does not exist
+
+            _instance = CreateInstance<XR_SkeletonPoserSettings>();
+                    
+            AssetDatabase.CreateAsset(_instance, "Assets/XRPoses/Resources/SkeletonPoserSettings.asset");
+            AssetDatabase.SaveAssets();
+        }
+        
+    }    
+}
