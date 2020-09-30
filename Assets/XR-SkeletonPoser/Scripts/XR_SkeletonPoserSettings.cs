ae5cf1e96a37558c15e3134985d5232d306c03d2
yellowyears <yellowyears200@gmail.com>
2020-09-30 17:01:42 +0100

    Fix instance overwrite bug
    Again...

--- Assets/XR-SkeletonPoser/Scripts/XR_SkeletonPoserSettings.cs
+++ Assets/XR-SkeletonPoser/Scripts/XR_SkeletonPoserSettings.cs
@@ -20,28 +20,35 @@ namespace yellowyears.SkeletonPoser
 
         private static void LoadInstance()
         {
-            if (_instance != null) return;
-            
-            // Load settings, from a folder in Assets/XRPoses called "Resources"
-            // _instance = (XR_SkeletonPoserSettings)AssetDatabase.LoadAssetAtPath("Assets/XRPoses/Resources", typeof(XR_SkeletonPoserSettings));
-            _instance = Resources.Load<XR_SkeletonPoserSettings>("XRPoses");
-
-            if (_instance != null) return;
-                
-            // If it still is null, then it does not exist
-            
-            _instance = CreateInstance<XR_SkeletonPoserSettings>();
-            
-            if(!AssetDatabase.IsValidFolder("Assets/XRPoses/Resources"))
+            if (_instance == null)
             {
-                AssetDatabase.CreateFolder("Assets/XRPoses", "Resources");
+                // Load settings, from a folder in Assets/XRPoses called "Resources"
+                // _instance = (XR_SkeletonPoserSettings)AssetDatabase.LoadAssetAtPath("Assets/XRPoses/Resources", typeof(XR_SkeletonPoserSettings));
+                _instance = Resources.Load<XR_SkeletonPoserSettings>("XRPoses");
+
+                // Debug.Log("Load instance");
+                
+                if (_instance == null)
+                {
+                    // If it still is null, then it does not exist
+                    
+                    _instance = CreateInstance<XR_SkeletonPoserSettings>();
+                    
+                    // Debug.Log("Create instance");
+                    
+                    if(!AssetDatabase.IsValidFolder("Assets/XRPoses/Resources"))
+                    {
+                        AssetDatabase.CreateFolder("Assets/XRPoses", "Resources");
+                    }
+                    
+                    AssetDatabase.CreateAsset(_instance, "Assets/XRPoses/Resources/SkeletonPoserSettings.asset");
+                    AssetDatabase.SaveAssets();
+                    // EditorUtility.SetDirty(_instance);
+                    
+                    SetDefaultValues();
+                    // Debug.Log("Set default values");
+                }
             }
-            
-            AssetDatabase.CreateAsset(_instance, "Assets/XRPoses/Resources/SkeletonPoserSettings.asset");
-            // AssetDatabase.SaveAssets();
-            // EditorUtility.SetDirty(_instance);
-            
-            SetDefaultValues();
         }
 
         private static void SetDefaultValues()
